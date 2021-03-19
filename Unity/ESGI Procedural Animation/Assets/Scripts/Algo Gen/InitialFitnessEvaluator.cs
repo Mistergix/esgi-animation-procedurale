5b21c50ad3d608ed29d5e40d6208ad2980a0dade
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2021-03-19 15:36:32 +0100

    Update InitialFitnessEvaluator.cs

--- Unity/ESGI Procedural Animation/Assets/Scripts/Algo Gen/InitialFitnessEvaluator.cs
+++ Unity/ESGI Procedural Animation/Assets/Scripts/Algo Gen/InitialFitnessEvaluator.cs
@@ -9,7 +9,10 @@ namespace ESGI.AlgoGen
     {
         public override float Fitness(Individual individual)
         {
-            return Random.Range(0, 11f);
+            List<int> listtest=individual.InterpretedGenotype(8)
+                float res = listtest[0]*0.3+listtest[1]*0.5+listtest[2]*0.5+listtest[3]*0.6 + listtest[4] * 0.6 + 0.9*(255-listtest[5]) + 0.8 * (255 - listtest[6]) + 0.8 * (255 - listtest[7]) + +0.9 * (255 - listtest[8]) + +0.9 * (255 - listtest[9])
+            return res
+
         }
     }
 }
